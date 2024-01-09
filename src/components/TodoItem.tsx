f51c1d03a675fe88cffedc46f24f15e1b7f46832
Tomi Miettinen <tomi.miettinen@tuni.fi>
2024-01-09 16:28:17 +0200

    Changes to TodoItem buttons and their icons.

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -9,16 +9,16 @@ const TodoItem: React.FC<TodoItemProps> = ({ text }) => {
     return (
         <li className="todo-list-item">
             <div className="todo-container-left">
-                <button className="drag-button">
-                    <img src="assets/icons/drag.png" className="drag-icon" alt="" />
+                <button className="li-button">
+                    <img src="/assets/icons/drag.png" className="li-icon" alt="" />
                 </button>
             </div>
             <div className="todo-container-center">
                 <span className="item-text">{text}</span>
             </div>
             <div className="todo-container-right">
-                <button className="delete-button">
-                    <img src="assets/icons/delete.png" className="delete-icon" alt="" />
+                <button className="li-button">
+                    <img src="/assets/icons/delete.png" className="li-icon" alt="" />
                 </button>
             </div>
         </li>
