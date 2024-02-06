124b04dde6c140d2ddc9cf6445607c5a8e35b4e9
Tomi Miettinen <tomi.miettinen@tuni.fi>
2024-02-06 17:43:11 +0200

    Refactor color mode system.

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,13 @@ function App() {
 
   const [todos, setTodos] = React.useState<string[]>([]);
   const [colorMode, setColorMode] = React.useState<'light' | 'dark'>(
-    () => (localStorage.getItem('colorMode') as 'light' | 'dark') || 'light'
+    () => (localStorage.getItem('color-mode') as 'light' | 'dark') || 'light'
   );
 
   // Initialize the todos state with the todos from local storage.
   React.useEffect(() => {
     const storedTodos = localStorage.getItem('todos');
     if (storedTodos) {
-      console.log('Todos loaded from local storage!');
       setTodos(JSON.parse(storedTodos));
     }
   }, []);
@@ -24,7 +23,6 @@ function App() {
    */
   React.useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
-    console.log('Todos updated in local storage!');
   }, [todos]);
 
   /**
@@ -33,25 +31,27 @@ function App() {
    * @param {('light' | 'dark')} colorMode The current color mode.
    */
   React.useEffect(() => {
-
     if (colorMode === 'dark') {
       document.body.classList.add('dark-mode');
-      localStorage.setItem('colorMode', 'dark');
-      console.log('Dark mode activated!');
     } else {
       document.body.classList.remove('dark-mode');
-      localStorage.setItem('colorMode', 'light');
-      console.log('Dark mode deactivated!');
     }
-
   }, [colorMode]);
+
+  /**
+   * 
+   * @param {('light' | 'dark')} mode The color mode to change to.
+   */
+  const changeColorMode = (mode: 'light' | 'dark') => {
+    localStorage.setItem('color-mode', mode);
+    setColorMode(mode);
+  }
 
   /**
    * @param newTodo The new todo to add to the list.
    */
   const addTodo = (newTodo: string) => {
     setTodos([...todos, newTodo]);
-    console.log('Todo added: ' + newTodo);
     localStorage.setItem('todos', JSON.stringify([...todos, newTodo]));
   };
 
@@ -61,12 +61,10 @@ function App() {
   const deleteTodo = (index: number) => {
     const newTodos = todos.filter((todo, i) => i !== index);
     setTodos(newTodos);
-    console.log('Todo deleted!');
     localStorage.setItem('todos', JSON.stringify(newTodos));
   };
 
   const saveTodo = (index: number, text: string) => {
-    console.log('Should be saving');
     let newTodos = [...todos];
     newTodos[index] = text;
     setTodos(newTodos);
@@ -77,7 +75,7 @@ function App() {
     <div id="app">
       
       {/* Add a navigation bar on top of the main TodoList. */}
-      <NavBar onSetColorMode={setColorMode} />
+      <NavBar onSetColorMode={changeColorMode} />
 
       {/* Make the main TodoList. */}
       <TodoList todos={todos} onAddTodo={addTodo} onDeleteTodo={deleteTodo} onSaveTodo={saveTodo} />
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,3 +1,4 @@
+import { on } from 'events';
 import React from 'react';
 
 interface NavItemProps {
@@ -7,23 +8,18 @@ interface NavItemProps {
 };
  
 const NavItem: React.FC<NavItemProps> = ({ icon, altText, onClickFunction }) => {
-    const [mode, setMode] = React.useState<'light' | 'dark'>('light');
 
-    /**
-     * When the button is clicked, toggle the color mode
-     * between light and dark. Then, call the parent's
-     * onClickFunction to visually update the color mode.
-     */
-    const handleClick = () => {
-        const newMode = mode === 'light' ? 'dark' : 'light';
-        localStorage.setItem('color-mode', newMode);
-        setMode(newMode);
-        onClickFunction(newMode);
+    const findMode = () => {
+        if (localStorage.getItem('color-mode') === 'dark') {
+            onClickFunction('light');
+        } else {
+            onClickFunction('dark');
+        }
     }
 
     return (
         <li className="nav-item">
-            <button className="nav-button" onClick={handleClick}>
+            <button className="nav-button" onClick={findMode}>
                 <img src={icon} alt={altText} style={{ width: '80%', height: '80%' }} />
             </button>
         </li>
